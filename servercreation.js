65baffda6299368718530b0c8a6dc2cabbbf9b10
srikar003 <srikarvuppala003@gmail.com>
2017-06-14 21:55:38 +0530

    modified

--- servercreation.js
+++ servercreation.js
@@ -1,14 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = "3200";
-
-const server = http.createServer((req,res) =>{
+fs.readFile('context.html',(err,html)=>{
+	if(err){
+		throw err;
+	}
+	const server = http.createServer((req,res) =>{
 	res.statusCode = 200;
-	res.setHeader('Content-type','text/plain');
-	res.end(`${2+3}`);
+	res.setHeader('Content-type','text/html');
+	res.write(html);
+	res.end();
 });
 
 server.listen(port,hostname,()=>{
 	console.log("server started on port "+port);
+});
+
 });
